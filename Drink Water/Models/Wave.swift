876a696c8728c75a6ddcc21b647e433932a2145c
AntonShiva <ryashinanthon@yandex.ru>
2023-11-06 20:41:30 +0300

    Добавил картинку стакана и цвет. Начал создавать структуру волны для отображенгия воды в стакане.

---
+++ Drink Water/Models/Wave.swift
@@ -0,0 +1,18 @@
+//
+//  Wave.swift
+//  Drink Water
+//
+//  Created by Anton Rasen on 06.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Wave :Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        <#code#>
+    }
+    
+    
+}
