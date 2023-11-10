e098c59ef8aab0ae4bcf39c82240c4171b6b396e
AntonShiva <ryashinanthon@yandex.ru>
2023-11-10 09:14:14 +0300

    создал эффект волны

--- Drink Water/Models/Wave.swift
+++ Drink Water/Models/Wave.swift
@@ -9,9 +9,34 @@ import Foundation
 import SwiftUI
 
 struct Wave :Shape {
+    var offset: Angle
+    var percent: Double
+    
+    var animatableData: Double {
+        get { offset.degrees }
+        set { offset = Angle(degrees: newValue) }
+    }
     
     func path(in rect: CGRect) -> Path {
-        <#code#>
+        var path = Path()
+        
+        let waveHeight = 0.015 * rect.height
+        let yOffset = CGFloat(1 - percent) * (rect.height - 4 * waveHeight) + 2 * waveHeight
+        let startAngle = offset
+        let endAngle = offset + Angle(degrees: 360)
+        
+        path.move(to: CGPoint(x: 0, y: yOffset + waveHeight * CGFloat(sin(offset.radians))))
+        
+        for angle in stride(from: startAngle.degrees, through: endAngle.degrees, by: 10) {
+            let x = CGFloat((angle - startAngle.degrees) / 360) * rect.width
+            path.addLine(to: CGPoint(x: x, y: yOffset + waveHeight * CGFloat(sin(Angle(degrees: angle).radians))))
+        }
+        
+        path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        path.addLine(to: CGPoint(x: 0, y: rect.height))
+        path.closeSubpath()
+        
+        return path
     }
     
     
