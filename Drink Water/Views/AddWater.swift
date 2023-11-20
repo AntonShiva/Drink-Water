ec33603358c2f114fc4d7f0d2194f41ea4cc4b55
AntonShiva <ryashinanthon@yandex.ru>
2023-11-20 12:23:29 +0300

    начал создавать экран напоминаний набросал кнопку добавления напоминания и два свича один для звука другой для самих напоминаний

--- Drink Water/Views/AddWater.swift
+++ Drink Water/Views/AddWater.swift
@@ -32,147 +32,168 @@ struct AddWater: View {
     }
     
     var body: some View {
-        ZStack {
-            Color.background
-                .ignoresSafeArea()
-            
-            VStack {
-                // Count label
-                Text("\(waterCount) ml")
-                    .padding(.top, 20.0)
-                    .foregroundStyle(Color.cyan)
-                    .font(.title)
+        NavigationView {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
                 
-                // MARK: Man and wave
-                ZStack(alignment: .center) {
-                    Rectangle()
-                        .fill(Color.manColor.opacity(0.8))
-                        .frame(width: 300, height: 400)
-                    
-                    
-                    
-                    Wave(offset: Angle(degrees: self.waveOffset.degrees), percent: percet / 95.0)
-                        .fill(Color.cyan)
-                        .frame(width: 300, height: 410)
-                        .offset(x: -30, y: 8)
-                    
-                    Wave(offset: Angle(degrees: self.waveOffset2.degrees), percent: percet / 95.0)
-                        .fill(Color.cyan)
-                        .opacity(0.5)
-                        .frame(width: 300, height: 410)
-                        .offset(x: 5, y: 8)
-                }
-                .mask {
-                    Image("man")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300, height: 400)
-                       
-                    
-                    
-                }
-//                .padding()
-                .onAppear {
-                    withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
-                        self.waveOffset = Angle(degrees: 360)
-                        self.waveOffset2 = Angle(degrees: -180)
-                    }
-                }
-                
-                //_____________________________________________
-                
-                Spacer()
-                    .frame(height: 20)
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Выбери порцию в мл.")
-                            .foregroundStyle(.cyan)
-                            .font(.system(size: 19))
-                            .frame(width: 200.0)
+                VStack {
+                    HStack() {
+                      
+                        Spacer()
                         
-                        Picker("Picker", selection: $selectedML) {
-                            ForEach(ml, id: \.self) {
-                                Text("\($0)")
-                            }
+                        NavigationLink(destination: Reminders()) {
+                      
+                            Image(systemName: "alarm.waves.left.and.right.fill")
+                                                    .foregroundStyle(.cyan)
+                                                    .font(.system(size: 30))
                         }
-                        .pickerStyle(PalettePickerStyle())
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.cyan, Color.blue]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(7)
-                        .frame(width: 205)
-                    }
-                    
-                    
-                    HStack {
-                        Image(systemName: "plus")
-                            .foregroundStyle(.cyan)
                         
-                        Button {
-                            if waterCount < dailyRate {
-                                let chislo = dailyRate / selectedML
-                                percet += Double(100 / chislo)
-                                waterCount += selectedML
-                            }
-                        } label: {
-                            Image("glass1")
-                                .resizable()
-                                .frame(width: 50, height: 50)
+                        
                             
                             
+                    }
+                    .padding(.horizontal, 20.0)
+                    .frame(maxWidth: .infinity)
+                    
+                    
+                    // Count label
+                    Text("\(waterCount) ml")
+                        .padding(.top, 20.0)
+                        .foregroundStyle(Color.cyan)
+                        .font(.title)
+                    
+                    // MARK: Man and wave
+                    ZStack(alignment: .center) {
+                        Rectangle()
+                            .fill(Color.manColor.opacity(0.8))
+                            .frame(width: 300, height: 400)
+                        
+                        
+                        
+                        Wave(offset: Angle(degrees: self.waveOffset.degrees), percent: percet / 95.0)
+                            .fill(Color.cyan)
+                            .frame(width: 300, height: 410)
+                            .offset(x: -30, y: 8)
+                        
+                        Wave(offset: Angle(degrees: self.waveOffset2.degrees), percent: percet / 95.0)
+                            .fill(Color.cyan)
+                            .opacity(0.5)
+                            .frame(width: 300, height: 410)
+                            .offset(x: 5, y: 8)
+                    }
+                    .mask {
+                        Image("man")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 400)
+                           
+                        
+                        
+                    }
+    //                .padding()
+                    .onAppear {
+                        withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                            self.waveOffset = Angle(degrees: 360)
+                            self.waveOffset2 = Angle(degrees: -180)
                         }
                     }
                     
-                }
-                .padding()
-                .frame(width: 320.0, height: 80)
-                .overlay(
-                  RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.cyan, lineWidth: 2)
-                )
-                
-                Spacer()
-                    .frame(height: 25)
-                
-                VStack(spacing: 5.0) {
-                    Text("Выбери свою дневную норму")
-                        .foregroundStyle(.cyan)
-                        .font(.system(size: 18))
+                    //_____________________________________________
+                    
+                    Spacer()
+                        .frame(height: 20)
+                    
                     HStack {
-                        
-                        Button {
-                            if dailyRate > 0 {
-                                dailyRate -= 100
-                            }
-                        } label: {
-                            Image(systemName: "minus.circle")
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Выбери порцию в мл.")
                                 .foregroundStyle(.cyan)
-                                .font(.system(size: 30))
+                                .font(.system(size: 19))
+                                .frame(width: 200.0)
+                            
+                            Picker("Picker", selection: $selectedML) {
+                                ForEach(ml, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(PalettePickerStyle())
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.cyan, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(7)
+                            .frame(width: 205)
                         }
                         
-                        Text("\(dailyRate) ml")
-                            .foregroundStyle(.cyan)
-                            .font(.system(size: 28))
                         
-                        Button {
-                            if dailyRate < 3000 {
-                                dailyRate += 100
-                            }
-                        } label: {
-                            Image(systemName: "plus.circle")
+                        HStack {
+                            Image(systemName: "plus")
                                 .foregroundStyle(.cyan)
-                                .font(.system(size: 30))
+                            
+                            Button {
+                                if waterCount < dailyRate {
+                                    let chislo = dailyRate / selectedML
+                                    percet += Double(100 / chislo)
+                                    waterCount += selectedML
+                                }
+                            } label: {
+                                Image("glass1")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                
+                                
+                            }
                         }
+                        
                     }
+                    .padding()
+                    .frame(width: 320.0, height: 80)
+                    .overlay(
+                      RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.cyan, lineWidth: 2)
+                    )
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    
+                    VStack(spacing: 5.0) {
+                        Text("Выбери свою дневную норму")
+                            .foregroundStyle(.cyan)
+                            .font(.system(size: 18))
+                        HStack {
+                            
+                            Button {
+                                if dailyRate > 0 {
+                                    dailyRate -= 100
+                                }
+                            } label: {
+                                Image(systemName: "minus.circle")
+                                    .foregroundStyle(.cyan)
+                                    .font(.system(size: 30))
+                            }
+                            
+                            Text("\(dailyRate) ml")
+                                .foregroundStyle(.cyan)
+                                .font(.system(size: 28))
+                            
+                            Button {
+                                if dailyRate < 3000 {
+                                    dailyRate += 100
+                                }
+                            } label: {
+                                Image(systemName: "plus.circle")
+                                    .foregroundStyle(.cyan)
+                                    .font(.system(size: 30))
+                            }
+                        }
+                        
+                    }
+                    .padding()
+                    .frame(width: 320.0, height: 80)
+                    .overlay(
+                      RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.cyan, lineWidth: 2)
+                    )
+                    
                     
                 }
-                .padding()
-                .frame(width: 320.0, height: 80)
-                .overlay(
-                  RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.cyan, lineWidth: 2)
-                )
-                
-                
             }
         }
     }
---
+++ Drink Water/Views/Reminders.swift
@@ -0,0 +1,75 @@
+//
+//  Reminders.swift
+//  Drink Water
+//
+//  Created by Anton Rasen on 17.11.2023.
+//
+
+import SwiftUI
+
+struct Reminders: View {
+    @State private var remindersOn = true
+    @State private var soundOn = true
+    
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Text("Напоминания")
+                    .padding(.vertical, 8.0)
+                    .foregroundStyle(.cyan)
+                    .font(.system(size: 25))
+                
+                VStack {
+                    
+                    Toggle("Напоминания Вкл/Выкл", isOn: $remindersOn)
+                        .foregroundStyle(.cyan)
+                        .toggleStyle(SwitchToggleStyle(tint: .cyan))
+                    
+                    Divider()
+                        .overlay(.cyan)
+                    
+                    Toggle("Звук", isOn: $soundOn)
+                        .foregroundStyle(.cyan)
+                        .toggleStyle(SwitchToggleStyle(tint: .cyan))
+                    
+                    
+                    
+                }
+                .padding()
+                .frame(width: 350.0, height: 100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.cyan, lineWidth: 2)
+                )
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Добавить напоминание")
+                            .foregroundStyle(.cyan)
+                            .font(.system(size: 20))
+                        Image(systemName: "plus.circle")
+                            .foregroundStyle(.cyan)
+                            .font(.system(size: 25))
+                    }
+                    .padding()
+                    .frame(width: 350.0, height: 50)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.cyan, lineWidth: 2)
+                    )                }
+               
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    Reminders()
+}
