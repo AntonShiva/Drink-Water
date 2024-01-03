a6054f3886ea58d8b94c013b6554e5aa17aef37f
AntonShiva <ryashinanthon@yandex.ru>
2024-01-03 13:32:37 +0300

    отредактивровал окно истории

--- Drink Water/Views/History.swift
+++ Drink Water/Views/History.swift
@@ -16,47 +16,97 @@ struct History: View {
     
     @State private var date = Date()
     
+    func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "MM LLLL yyyy"  // Формат отображения месяца и года на русском
+
+        return dateFormatter.string(from: date)
+    }
     
     var body: some View {
-        
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
             VStack {
+                VStack {
+                    RoundedRectangle(cornerRadius: 15)
+                                      .fill(Color.cyan)
+                                      .frame(width: 370, height: 310, alignment: .center)
+                                      .overlay(
+                                  DatePicker(
+                                      "Start Date",
+                                      selection: $date,
+                                      displayedComponents: [.date]
+                                          
+                                  )
+                                  .environment(\.locale, Locale(identifier: "ru_RU"))
+                                  .frame(width: 340, height: 294, alignment: .center)
+                                  .clipped()
+                                  .tint(.bly)
+                                  .datePickerStyle(.graphical)
+                                  .background(Color.cyan)
+                                  .padding(20)
+                                 
+                                )
+                                      .padding(15)
+                    
+                  
+                }
                 
-              
-                DatePicker(
-                    "Start Date",
-                    selection: $date,
-                    displayedComponents: [.date]
-                )
-                .datePickerStyle(.graphical)
-                
-                
-                
-                List {
-                    ForEach(waterConsumptionByDate.sorted(by: { $0.date < $1.date })) { item in
-                        if item.date.formatted(date: .complete, time: .omitted) == date.formatted(date: .complete, time: .omitted) {
-                            Section(header: Text(item.date.formatted(date: .long, time: .omitted))) {
-                                ForEach(item.vremia.indices, id: \.self) { index in
-                                    HStack {
-                                        Image("glass1")
-                                            .resizable()
-                                            .frame(width: 40, height: 40)
-                                        Text("\(item.porcaica[index])")
-                                        Spacer()
-                                        Text(item.vremia[index])
-                                            .padding(.trailing, 50.0)
+                VStack {
+                    List {
+                        ForEach(waterConsumptionByDate.sorted(by: { $0.date < $1.date })) { item in
+                            if item.date.formatted(date: .complete, time: .omitted) == date.formatted(date: .complete, time: .omitted) {
+                                Section(header: Text(formatDate(item.date))
+                                                                            .font(.title)
+                                                                            .foregroundColor(.cyan)
+                                                                            
+                                                                            .frame(maxWidth: .infinity, alignment: .center)
+                                                                            .textCase(.none))
+                                {
+                                    ForEach(item.vremia.indices, id: \.self) { index in
+                                        HStack {
+                                            Image("glass1")
+                                                .resizable()
+                                                .frame(width: 40, height: 40)
+                                                .padding(.leading, 15.0)
+                                            Text("-  \(item.porcaica[index]) мл")
+                                                .foregroundColor(.cyan)
+                                                
+                                            
+                                            Spacer()
+                                            
+                                            Text(item.vremia[index])
+                                                .foregroundColor(.cyan)
+                                                .padding(.trailing, 20.0)
+                                        }
+                                       
+                                      
+                                        .frame(width: 300.0, height: 60)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.cyan, lineWidth: 2)
+                                        )
                                     }
-                                    .padding(.horizontal, 50)
+                                    .padding(.horizontal, 20)
+                                    
                                 }
                             }
+                            
                         }
-                        
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
                     }
+                    .scrollContentBackground(.hidden)
+                    
+                    Spacer()
                 }
                 
                 
             }
-            
-    
+            .padding(.top, 30)
+        }
     }
 }
 
